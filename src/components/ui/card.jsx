b4140758ca29bc685ac3c0e1e0cafacbfa96268f
Guido Pawluk <guidopawluk@gmail.com>
2026-08-30 15:35:11 -0300

    card.jsx: CardContent usa px-md pb-md para que pt override funcione

--- src/components/ui/card.jsx
+++ src/components/ui/card.jsx
@@ -26,7 +26,7 @@ const CardDescription = React.forwardRef(({ className, ...props }, ref) => (
 CardDescription.displayName = 'CardDescription'
 
 const CardContent = React.forwardRef(({ className, ...props }, ref) => (
-  <div ref={ref} className={cn('p-md pt-0', className)} {...props} />
+  <div ref={ref} className={cn('px-md pb-md', className)} {...props} />
 ))
 CardContent.displayName = 'CardContent'
 
